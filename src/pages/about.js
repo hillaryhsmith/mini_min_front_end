afa35ae1a5c65fc437e84f5de3ea99c566f037b6
Hillary Smith <hillaryhsmith@gmail.com>
2022-08-14 12:23:53 -0700

    fill in text for home and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,40 @@
 const About = () => {
-    return <h1>About</h1>;
+    return (
+        <div>
+            <h1>About</h1>
+            <h2>Welcome to MiniMin!</h2>
+            <p>Begin by visiting our Learn Minerals page. Here you will learn information about the 
+                physical and chemical properties of minerals and see photos of hand samples. You will also 
+                get hints on how to identify mineral. Remember to push the "I've learned this mineral" button 
+                when you want to add it to your list of minerals to review.</p>
+            <p>Once you have learned some minerals, visit the Review Minerals page to study the information you 
+                have learned. Navigate to specific minerals using the list at the top of the page, or scroll down to see a randomly 
+                selected mineral for you to review. Clicking the "Show me a new mineral to review" button will display information about 
+                a different randomly selected mineral. If you are shown a mineral to review that you feel you have forgotten since you 
+                last studied feel free to use the "I've forgotten this mineral" button to move it out of your collection of learned minerals. 
+                It will be presented to you again when you are on the Learn Minerals page.
+            </p>
+            <p>Once you have learned a small collection of minerals, the Quiz page will become available to you. A short quiz of 5 questions will 
+                be generated to test your knowledge of your learned minerals, including your ability to identify them from a photo. Feedback after 
+                each question and a score at the end will be provided, but only to help you see how you are doing, and let you know what minerals you 
+                need to spend more time studying. A new quiz will be generated with the "Give me a different quiz" button, which is offered when your 
+                quiz score is shown.
+            </p>
+            <p>
+                Enjoy learning about minerals, wherever you are, on MiniMin! 
+            </p>
+
+            <h2>Credits</h2>
+            <p>Photos of handsamples were accessed from the USGS Denver Library Photographic Collection. The photographer was Andrew Silver and the 
+                handsamples are from the mineral collection of Brigham Young University Department of Geology, Provo, Utah.
+            </p>
+            <p>Mineral information provided was collected from a variety of sources, including mindat.org and the text Earth Materials: Introduction 
+                to Mineralogy and Petrologyy by Cornelis Klein and Anthony Philpotts.  
+            </p>
+            <p>The crystal icon was obtained from flaticon.com and is attributed to Freepik.
+            </p>
+        </div>
+    );
 };
 
 export default About;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,24 @@
 const Home = () => {
-    return <h1>Home</h1>;
+    return (    
+        <div>
+            <h1>Home</h1>
+            <h2>Welcome to MiniMin, a mobile web app that helps you learn to identify the most common minerals!</h2>
+            <p>MiniMin was created as Capstone Project for Ada Developer's Academy. 
+                It allowed the creator to combine her previous training in  Earth sciences 
+                and her passion for informal education into a app that shows off her new softwared developement skills.</p>
+            <p>The intended audience for this app includes hobbyists interested in learning to identify new minerals, as well as 
+                college students and secondary school students who may be interested in a future career in the geosciences.</p>
+            <p>Knowledge about Earth science is key to understanding the current climate crisis. However, Earth science is lacking 
+                in most K-12 curricula. Only 16% of US states require high school students to take a course that includes Earth 
+                and space science topics (Earth and Space Sciences Education in U.S. Secondary Schools: Key Indicators and Trends, 2018).</p> 
+            <p>Access to informal education about Earth science increases understanding and reduces the knowledge gap, while helping us all 
+                to appreciate and respect our planet.</p>
+
+            <a href="https://www.americangeosciences.org/sites/default/files/SecondaryEdu2018Report_20pgPlusCovers_PrintRes_120618.pdf">
+                Earth and Space Sciences Education in U.S. Secondary Schools: Key Indicators and Trends
+            </a>
+        </div>
+    );
 };
 
 export default Home;
